ce0fc007fef1a7c42f4a93819973d1c4fc86c06e
aleksandre <aleksandrekasrashvili@gmail.com>
2025-12-10 23:36:09 +0400

    project start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+
+  return (
+    <div className="
+    w-screen h-screen flex justify-items-center items-center
+    lg:bg-[url('/background-desktop.png')] 
+    md:bg-[url('/background-tablet.png')]
+    bg-[url('/background-mobile.png')]
+    bg-cover bg-center 
+    ">
+
+    </div>
+  )
+}
+
+export default App
