f0c2e0e1f40ccbbfaedf5ca5473642d8e7185e79
aleksandre <aleksandrekasrashvili@gmail.com>
2025-12-13 00:03:04 +0400

    just learned adding custom font to tailwind with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+import Form from "./components/Form"
+
+
+
 function App() {
 
 
   return (
     <div className="
-    w-screen h-screen flex justify-items-center items-center
+    w-screen h-screen flex justify-center items-center
     lg:bg-[url('/background-desktop.png')] 
     md:bg-[url('/background-tablet.png')]
     bg-[url('/background-mobile.png')]
     bg-cover bg-center 
-    ">
-
+    relative
+    "
+    >
+      <Form />
+      <img className="absolute top-0 left-0 w-full h-full z-0" src="/pattern-lines.svg" alt="lines" />    
+      <img className="absolute top-2/4 right-2/6  z-10" src="/pattern-circle.svg" alt="lines" />    
+      <picture className="absolute bottom-0 left-0 z-20">
+        <source media="(min-width:1024px)" srcSet="/pattern-squiggly-line-bottom-desktop.svg" />
+        <img src="/pattern-squiggly-line-bottom-mobile-tablet.svg" alt="squiggly-lines" />
+      </picture>
+      <img className="absolute top-0 right-0 w-[40%]" src="public\pattern-squiggly-line-top.svg" alt="lines-top" />
     </div>
   )
 }
 
 export default App
+ 
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Form(){
+    return(
+        <div className="lg:w-[40%] px-4 flex flex-col z-50">
+            <img className="h-10" src="/logo-full.svg" alt="coding_conf_logo" />
+            <h1 className="text-zinc-50 lg:text-[3rem] md:text-[1.5rem] text-[1.5rem]  font-bold text-center font-inconsolata">Your Journey to Coding Conf 2025 Starts Here!</h1>
+            <p>Secure your spot at next year's biggest coding conference.</p>
+        </div>
+    )
+}
